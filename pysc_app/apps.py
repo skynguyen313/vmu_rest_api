a8b6a34aa70c4a6f7e6dafe3361c74cdcaa32e52
skynguyen313 <skynguyen313>
2024-10-03 23:56:43 +0700

    change default auto field

--- pysc_app/apps.py
+++ pysc_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class PyscAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.AutoField'
     name = 'pysc_app'
