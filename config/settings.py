528922ec6be424344e3c4879090d15745e10993b
skynguyen313 <skynguyen313>
2024-10-04 16:37:05 +0700

    add reame.md

--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+AUTH_USER_MODEL = 'user.User'
